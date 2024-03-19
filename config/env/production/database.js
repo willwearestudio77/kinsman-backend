10bb9c39a0c6fb91177fe0c94f2a19fd8313d096
WILLIAM ELLIS <will@wearestudio77.com>
2024-03-19 13:27:33 -0600

    fix on server file

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,16 +1,16 @@
 module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        user: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: {
-          ca:env('DATABASE_SSL_CA'),
-        },
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: {
+        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
       },
-      debug: false,
     },
-  });
+    debug: false,
+  },
+});
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+      provider: 'digitalocean',
+      providerOptions: {
+        key: env('DO_SPACES_ACCESS_KEY'),
+        secret: env('DO_SPACES_SECRET_KEY'),
+        endpoint: env('DO_SPACES_ENDPOINT'),
+        space: env('DO_SPACES_BUCKET'),
+        directory: env('DO_SPACES_DIRECTORY')
+      }
+    }
+  });
